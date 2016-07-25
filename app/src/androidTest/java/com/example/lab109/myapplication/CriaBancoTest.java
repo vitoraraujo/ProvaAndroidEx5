419bf8d7751072d607909c4f0999033e2de41151
vitoraraujo <vitoraraujow@gmail.com>
2016-07-25 15:20:31 -0300

    Questão 5

---
+++ app/src/androidTest/java/com/example/lab109/myapplication/CriaBancoTest.java
@@ -0,0 +1,10 @@
+package com.example.lab109.myapplication;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lab109 on 25/07/16.
+ */
+public class CriaBancoTest {
+
+}
---
+++ app/src/main/java/com/example/lab109/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.lab109.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class CriaBanco extends SQLiteOpenHelper {
+    private static final String NOME_BANCO = "banco.db";
+    private static final String TABELA = "contatos";
+    private static final String ID = "_id";
+    private static final String NOME = "nome";
+    private static final String TELEFONE = "telefone";
+    private static final String EMAIL = "email";
+    private static final int VERSAO = 1;
+
+    public CriaBanco(Context context){
+        super(context, NOME_BANCO, null, VERSAO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE"+TABELA+"("
+                + ID + "integer primary key autoincrement,"
+                + NOME + "text,"
+                + TELEFONE + "text,"
+                + EMAIL + "text"
+                +")";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
